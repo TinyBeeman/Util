566e8853330c80f891b0776ce2b7eedacee75d1a
Tony Beeman <53918474+TinyBeeman@users.noreply.github.com>
2022-09-19 10:44:00 -0700

    Fix tab issue.
    Minor change to fix tabs

--- Python/sd_user_scripts/replace_tokens_with_list.py
+++ Python/sd_user_scripts/replace_tokens_with_list.py
@@ -58,29 +58,29 @@ class Script(scripts.Script):
         return [help_label, prompt_txt]
 
 
-def run(self, p, help_label, txt: str):
-    lines = [x.strip() for x in txt.splitlines()]
-    lines = [x for x in lines if (x.find(',') >= 0)]
-    if (len(lines) == 0):
-        return
+    def run(self, p, help_label, txt: str):
+        lines = [x.strip() for x in txt.splitlines()]
+        lines = [x for x in lines if (x.find(',') >= 0)]
+        if (len(lines) == 0):
+            return
 
-    replacer = TokenReplacer(lines)
-    prompts = replacer.createPrompts(p.prompt)
+        replacer = TokenReplacer(lines)
+        prompts = replacer.createPrompts(p.prompt)
 
-    img_count = len(prompts) * p.n_iter
-    batch_count = math.ceil(img_count / p.batch_size)
-    loop_count = math.ceil(batch_count / p.n_iter)
-    print(f"Will process {img_count} images in {batch_count} batches.")
+        img_count = len(prompts) * p.n_iter
+        batch_count = math.ceil(img_count / p.batch_size)
+        loop_count = math.ceil(batch_count / p.n_iter)
+        print(f"Will process {img_count} images in {batch_count} batches.")
 
-    p.do_not_save_grid = True
+        p.do_not_save_grid = True
 
-    state.job_count = batch_count
+        state.job_count = batch_count
 
-    images = []
-    for loop_no in range(loop_count):
-        state.job = f"{loop_no + 1} out of {loop_count}"
-        p.prompt = prompts[loop_no*p.batch_size:(loop_no+1)*p.batch_size] * p.n_iter
-        proc = process_images(p)
-        images += proc.images
+        images = []
+        for loop_no in range(loop_count):
+            state.job = f"{loop_no + 1} out of {loop_count}"
+            p.prompt = prompts[loop_no*p.batch_size:(loop_no+1)*p.batch_size] * p.n_iter
+            proc = process_images(p)
+            images += proc.images
 
-    return Processed(p, images, p.seed, "")
+        return Processed(p, images, p.seed, "")
